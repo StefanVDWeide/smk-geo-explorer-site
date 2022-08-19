3fa27cb8b446b81697cb67f13179a9f3b1d64bc3
stefanvdWeide <stefan.vander.weide@hotmail.com>
2022-08-19 18:39:54 +0200

    Removed random letter in code

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container min-h-screen mx-auto pb-24 px-6 md:px-12 font-gtflexa">s
+  <div class="container min-h-screen mx-auto pb-24 px-6 md:px-12 font-gtflexa">
     <navbar-component />
     <div>
       <NuxtPage />
