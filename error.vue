fe4ea466d6a53f96b0da5a8110e1a7d4a38cea13
stefanvdWeide <stefan.vander.weide@hotmail.com>
2022-08-19 20:27:40 +0200

    Added error page

---
+++ error.vue
@@ -0,0 +1,11 @@
+<template>
+    <button @click="handleError">Clear errors</button>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    error: Object
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
