b492cdf661ab709f50fd36e59d882dda02b37860
stefanvdWeide <stefan.vander.weide@hotmail.com>
2022-08-19 20:30:13 +0200

    Added styling to the error page

--- error.vue
+++ error.vue
@@ -1,5 +1,10 @@
 <template>
-    <button @click="handleError">Clear errors</button>
+    <div>
+        <h1 class="text-2xl">Oops! Looks like an error occured. Click the button below to go back to the homepage</h1>
+        <button @click="handleError"
+            class="text-black  bg-transparent  px-4 py-2 border-2 rounded border-black hover:bg-black  hover:text-white">Back
+            home</button>
+    </div>
 </template>
 
 <script lang="ts" setup>
